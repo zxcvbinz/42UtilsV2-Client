256644e4fdab9c68924626a4408ffd4aa168257b
Davide Lanotte <dlanotte@e3r3p3.42roma.it>
2021-11-12 00:10:20 +0100

    fix

--- exec/source/install.sh
+++ exec/source/install.sh
@@ -2,7 +2,12 @@
 
 normal=`tput sgr0`
 bold=`tput bold`
-
+orange=`tput setaf 172`
+green=`tput setaf 2`
+magenta=`tput setaf 9`
+WARN="${bold}[${orange}WARNING${normal}${bold}] "
+HINT="${bold}[${green}HINT${normal}${bold}] "
+COMPLETE="${bold}[${magenta}COMPLETE${normal}${bold}] "
 isInstalled() {
     if [ ! -d $HOME/.42utils ]; then
         return 0
@@ -11,29 +16,40 @@ isInstalled() {
     fi
 }
 
+installOhMyZsh() {
+    if [ ! -d $HOME/.oh-my-zsh ]; then
+        git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+        cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+        echo "${WARN}Installed oh-my-zsh"
+    fi
+    echo "export PATH=\"\$HOME/.42Utils/exec/:\$PATH\"" >> ~/.zshrc  
+}
+
 installUtils() {
     if isInstalled; then
         git clone git@github.com:zxcvbinz/42Utils-v2.git $HOME/.42utils
+        installOhMyZsh
         if ! isInstalled; then
-            clear
-            echo "\n${bold}42Utils has been installed..\n\n- Start with '42Hub' command\n${normal}"
+            echo "${WARN}${COMPLETE}42Utils has been installed..\n\n${HINT}- Start with '42Hub' command\n${normal}"
         fi
     else
-        echo "\n${bold}42Utils has been installed..\n\n- Start with '42Hub' command\n${normal}"
+        echo "${WARN}42Utils has been installed..\n\n${HINT}- Start with '42Hub' command\n${normal}"
     fi
     
 }
 
-Warnining() {
-    if ! read -q "choice?${bold}This tool overwrite your zsh configuration${normal}, are you sure? y/Y: "; then
-        return 1
-    else 
+Warning() {
+    if read -q "choice?${WARN}This tool overwrite your zsh configuration${normal}, are you sure? y/Y: "; then
+        echo "\n"
         return 0
+    else 
+        echo "\n"
+        return 1
     fi
 }
 
-if Warnining; then
+if Warning; then
     installUtils
 else
-    echo "\nAbort."
+    echo "Abort."
 fi
